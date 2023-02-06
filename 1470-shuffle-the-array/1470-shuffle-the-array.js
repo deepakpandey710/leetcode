1c8a620cb65085d930dc85497f62a70546606ce5
deepakpandey710 <87386712+deepakpandey710@users.noreply.github.com>
2023-02-06 12:04:15 +0530

    Time: 92 ms (29.26%), Space: 43.8 MB (95.57%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -1,12 +1,17 @@
-class Solution {
-public:
-    vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>ans;
-        int i=0,j=n;
-        while(i<n){
-            ans.push_back(nums[i++]);
-            ans.push_back(nums[j++]);
-        }
-        return ans;
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    let m = nums.length;
+    for (let i = n; i < m; i++) {
+        nums[i] = nums[i] * 1024 + nums[i - n];
     }
+    let index = 0;
+    for (let i = n; i < m; i++, index += 2) {
+        nums[index] = nums[i] % 1024;
+        nums[index + 1] = Math.floor(nums[i] / 1024);
+    }
+    return nums;
 };
