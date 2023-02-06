53134e146f8a16930b6e282ae57515eb25e1780e
deepakpandey710 <87386712+deepakpandey710@users.noreply.github.com>
2023-02-06 11:57:01 +0530

    Time: 10 ms (31.76%), Space: 9.7 MB (87.13%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int>ans;
-        int i=0,j=n;
-        while(i<n){
-            ans.push_back(nums[i++]);
-            ans.push_back(nums[j++]);
+        int m=nums.size();
+        for(int i=n;i<m;i++){
+            nums[i]=nums[i]*1024+nums[i-n];
         }
-        return ans;
+        int index=0;
+        for(int i=n;i<m;i++,index+=2){
+            nums[index]=nums[i]%1024;
+            nums[index+1]=nums[i]/1024;
+        }
+        return nums;
     }
 };
