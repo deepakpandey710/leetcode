b45caa5da40638458fe54c2229a32f26fa61ca0c
deepakpandey710 <87386712+deepakpandey710@users.noreply.github.com>
2023-02-10 23:03:17 +0530

    Time: 183 ms (18.08%), Space: 50.5 MB (5.32%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -3,7 +3,7 @@ public:
 
     int maxDistance(vector<vector<int>>& grid) {
         int n=grid.size();
-        vector<vector<int>>vis(n,vector<int>(n,0)),dist(n,vector<int>(n,0));
+        vector<vector<int>>vis(n,vector<int>(n,0));
         queue<vector<int>>q;
         int ans=0;
         for(int i=0;i<n;i++ ){
@@ -21,18 +21,13 @@ public:
         while(!q.empty()){
             vector<int>x=q.front();
             q.pop();
-            dist[x[0]][x[1]]=x[2];
+            ans=max(ans,x[2]);
             for(int i=0;i<4;i++){
                 int l=x[0]+d[i],m=x[1]+d[i+1];
                 if(l<n && m<n && l>=0 && m>=0 && !vis[l][m]){
                     vis[l][m]=1;
                     q.push({l,m,x[2]+1});
                 }
-            }
-        }
-        for(int i=0;i<n;i++ ){
-            for(int j=0;j<n;j++){
-                ans=max(ans,dist[i][j]);
             }
         }
         return ans;
