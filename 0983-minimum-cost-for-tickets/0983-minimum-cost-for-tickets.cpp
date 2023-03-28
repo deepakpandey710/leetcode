b88121ae67c972735628181b597c2ad20a432b18
deepakpandey710 <87386712+deepakpandey710@users.noreply.github.com>
2023-03-28 23:28:57 +0530

    Time: 3 ms (80.92%), Space: 9.8 MB (30.82%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -3,12 +3,11 @@ public:
     int f(int i,vector<int>& days, vector<int>& costs,vector<int>&dp){
         if(i>=days.size())return 0;
         if(dp[i]!=-1)return dp[i];
-        int one=costs[0]+f(i+1,days,costs,dp);
-        int seven=1e8,month=1e8;
         int j=lower_bound(days.begin(),days.end(),days[i]+7)-days.begin();
         int k=lower_bound(days.begin(),days.end(),days[i]+30)-days.begin();
-        seven=costs[1]+f(j,days,costs,dp);
-        month=costs[2]+f(k,days,costs,dp);
+        int one=costs[0]+f(i+1,days,costs,dp);
+        int seven=costs[1]+f(j,days,costs,dp);
+        int month=costs[2]+f(k,days,costs,dp);
         return dp[i]= min({one,seven,month});
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
