e67e2bf9579450486ed602dff2941054e8e15f9f
deepakpandey710 <87386712+deepakpandey710@users.noreply.github.com>
2023-04-03 22:39:01 +0530

    Time: 80 ms (88.31%), Space: 41.9 MB (93.02%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        int cnt=people.size(),i=0,j=people.size()-1;
-        sort(people.begin(),people.end());
-        while(i<=j){
-            while(j>i && people[i]+people[j]>limit){
-                j--;
+        sort(people.begin(), people.end()); // sort the people by weight
+        int left = 0, right = people.size() - 1; // initialize two pointers at the beginning and end of the array
+        int boats = 0; // initialize the number of boats to 0
+        while (left <= right) { // loop until the two pointers cross
+            if (people[left] + people[right] <= limit) { // if the sum of the weights of the two people is at most limit
+                left++; // move the left pointer to the next person
+                right--; // move the right pointer to the previous person
+            } else { // otherwise, the heavier person must take a boat alone
+                right--; // move the right pointer to the previous person
             }
-            if(i<j && people[i]+people[j]<=limit)
-                cnt--;
-            i++;j--;
+            boats++; // increment the number of boats
         }
-        return cnt;
+        return boats; // return the final number of boats
     }
 };
