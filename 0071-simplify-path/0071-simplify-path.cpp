c4cfb5321a9f1757223cbcb53df1b2650fc2ce85
deepakpandey710 <87386712+deepakpandey710@users.noreply.github.com>
2023-04-12 23:46:22 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (66.72%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string ans="";
+        int i=0,n=path.size();
+        vector<string>st;
+        while(i<n){
+            string cur="";
+            while(i<n && path[i]!='/'){
+                cur+=path[i];
+                i++;
+            }
+            if(cur==".."){
+                if(st.size())
+                    st.pop_back();
+            }
+            else if(cur.size()&&cur!="."){
+                st.push_back(cur);
+            }
+            i++;
+        }
+        for(auto it:st){
+            ans+='/'+it;
+        }
+        return ans.size()?ans:"/";
+        // if(path[0]!='/'){
+        //     ans+='/';
+        //     i=0;
+        // }
+        // while(i<n){
+            // string cur="";
+            // while(i<n && path[i]!='/'){
+            //     cur+=path[i];
+            //     i++;
+            // }
+        //     if(cur=='.'||)
+        //     i++;
+        // }
+    }
+};
