4e01063a2a91f38a7116aae47915fd4769d87334
Deepak pandey <87386712+deepakpandey710@users.noreply.github.com>
2024-02-14 12:43:40 +0530

    Time: 152 ms (63.15%), Space: 132.4 MB (35.46%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -5,13 +5,23 @@ public:
         int n=nums.size(),i=0,j=0,p=1;
         while(i<n || j<n){
             if(p){  
-                while(i<n && nums[i]<0)i++;
-                if(i<n)ans.push_back(nums[i]);
-                i++;
+                while(i<n){
+                    if(nums[i]>0){
+                       ans.push_back(nums[i]);
+                        i++;
+                        break;
+                    }
+                    i++;
+                }
             }else{
-                while(j<n && nums[j]>0)j++;
-                if(j<n)ans.push_back(nums[j]);
-                j++;
+                while(j<n){
+                    if(nums[j]<0){
+                       ans.push_back(nums[j]);
+                        j++;
+                        break;
+                    }
+                    j++;
+                }
             }
             p=!p;
         }
