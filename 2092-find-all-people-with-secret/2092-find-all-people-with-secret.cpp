9a307110dec2f1b4c5be7c8eb8db25318bbc8840
Deepak pandey <87386712+deepakpandey710@users.noreply.github.com>
2024-02-24 18:56:12 +0530

    Time: 372 ms (94.67%), Space: 142.9 MB (87.71%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,53 +1,56 @@
+class UnionFind{
+    vector<int>parent;
+public:
+    UnionFind(int n){
+        parent.resize(n,0);
+        for(int i=1;i<n;i++){
+            parent[i]=1+parent[i-1];
+        }
+    }
+    void connect(int a,int b){
+        parent[find(b)]=find(a);
+    }
+    
+    int find(int i){
+        return parent[i]==i?i:(parent[i]=find(parent[i]));
+    }
+    
+    bool connected(int b){
+        return find(0)==find(b);
+    }
+    
+    void reset(int a){
+        parent[a]=a;
+    }
+};
+
 class Solution {
 public:
-//     static bool cmp(vector<int>&v1,vector<int>&v2){
-//         return v1[2]<v2[2];
-//     }
-//     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-//         sort(meetings.begin(),meetings.end(),cmp);
-//         set<int>st({0,firstPerson});
-//         for(auto &it:meetings){
-//             if(st.find(it[0])!=st.end() || st.find(it[1])!=st.end()){
-//                 st.insert(it[0]);
-//                 st.insert(it[1]);
-//             }
-//         }
-//         vector<int>ans(st.begin(),st.end());
-//         return ans;
-        
-//     }
-    
-
+    static bool cmp(vector<int>&v1,vector<int>&v2){
+        return v1[2]<v2[2];
+    }
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        map<int,map<int,vector<int>>>mpp;
-        for(auto &it:meetings){
-            mpp[it[0]][it[2]].push_back(it[1]);
-            mpp[it[1]][it[2]].push_back(it[0]);
-        }
-        queue<vector<int>>q;
-        q.push({firstPerson,0});
-        q.push({0,0});
-        set<int>st({0,firstPerson});
-        vector<int>vis(n,INT_MAX);
-        vis[0]=0;
-        vis[firstPerson]=0;
-        while(!q.empty()){
-            auto f=q.front();
-            q.pop();
-            for(auto &time:mpp[f[0]]){
-                if(time.first>=f[1]){
-                    for(auto &it:time.second){
-                        if(vis[it]>time.first){
-                            vis[it]=time.first;
-                            q.push({it,time.first});
-                            st.insert(it);
-                        }
-                    }
-                }
+        sort(meetings.begin(),meetings.end(),cmp);
+        UnionFind uf(n);
+        uf.connect(0,firstPerson);
+        vector<int>ppl,ans;
+        for(int i=0,m=meetings.size();i<m;){
+            ppl.clear();
+            int time=meetings[i][2];
+            for(;i<m && time==meetings[i][2];++i){
+                uf.connect(meetings[i][0],meetings[i][1]);
+                ppl.push_back(meetings[i][0]);
+                ppl.push_back(meetings[i][1]);
+            }
+            for(auto &it:ppl){
+                if(!uf.connected(it))uf.reset(it);
             }
         }
-        vector<int>ans(st.begin(),st.end());
-        return ans;
         
+        for(int i=0;i<n;i++){
+            if(uf.connected(i))ans.push_back(i);
+        }
+        return ans;
     }
+    
 };
