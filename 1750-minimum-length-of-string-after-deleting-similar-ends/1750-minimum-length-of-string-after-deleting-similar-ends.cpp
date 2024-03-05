f0f54345dfab1a22a85c84ecee5a966f1af7eb39
Deepak pandey <87386712+deepakpandey710@users.noreply.github.com>
2024-03-05 20:42:06 +0530

    Time: 37 ms (5.17%), Space: 14 MB (13.45%) - LeetHub

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j){
+            int l=i+1,m=j-1;
+            while(l<j && s[i]==s[l]){
+                l++;
+            }
+            while(i<m && s[j]==s[m]){
+                m--;
+            }
+            if(s[i]==s[j]){
+                i=l;
+                j=m;
+            }else 
+                break;
+        }
+        return max(j-i+1,0);
+    }
+};
