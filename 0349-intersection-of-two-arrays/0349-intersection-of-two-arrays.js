fa62ca9b9d6549d20a849680e0e5bf77f8effd60
Deepak pandey <87386712+deepakpandey710@users.noreply.github.com>
2024-03-11 00:12:58 +0530

    Time: 63 ms (18.89%), Space: 51.3 MB (10.01%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -6,5 +6,5 @@
 var intersection = function(nums1, nums2) {
     const st1=new Set(nums1);
     const st2=new Set(nums2);
-    return Array.from(new Set([...st1].filter((x)=>st2.has(x))));
+    return Array.from(new Set([...st1].filter((x)=>{return st2.has(x)})));
 };
