3b5c65be1fc71f32847fe5d1827c2c603ceaa838
Deepak pandey <87386712+deepakpandey710@users.noreply.github.com>
2024-03-11 18:28:09 +0530

    Time: 52 ms (74.22%), Space: 49.1 MB (67.58%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.js
+++ 0791-custom-sort-string/0791-custom-sort-string.js
@@ -4,29 +4,6 @@
  * @return {string}
  */
 var customSortString = function(order, s) {
-    const arr={};
-    for (const ch of s){
-        if(arr[ch])
-            arr[ch]++;
-        else
-            arr[ch]=1;
-    }
-    console.log(arr);
-    let ans="";
-    for (const ch of order){
-        console.log(ch);
-        let n=arr[ch];
-        for(let i=0;i<n;i++){
-            ans+=ch;
-        }
-        arr[ch]=0;
-    }
-    console.log(ans);
-    for (const it in arr){       
-        let n=arr[it];
-        for(let j=0;j<n;j++){
-            ans+=it;
-        }
-    }
-    return ans;
+     const sortedChars = s.split('').sort((a, b) => order.indexOf(a) - order.indexOf(b));
+    return sortedChars.join('');
 };
