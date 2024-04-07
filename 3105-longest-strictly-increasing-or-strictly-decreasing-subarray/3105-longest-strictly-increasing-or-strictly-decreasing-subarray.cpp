c0820c6368bdbc99d5b0c9967e209ffa9a5a633d
Deepak pandey <87386712+deepakpandey710@users.noreply.github.com>
2024-04-07 23:55:49 +0530

    Time: 4 ms (71.43%), Space: 26.7 MB (42.86%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -2,26 +2,43 @@ class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
         int i=0,n=nums.size(),ans=1;
-        if(n<2)return n;
-        
-        while(i<n){
-            while(i+1<n && nums[i]==nums[i+1]){
-                    i++;
-            }
-            int j=i+1;
-            if(j<n && nums[j]>nums[j-1]){
-                while(j<n && nums[j]>nums[j-1]){
-                    j++;
+        for(int i=0;i<n;i++){
+            int cnt=1;
+            for(int j=i+1;j<n;j++){
+                if(nums[j]<=nums[j-1]){
+                    break;
                 }
-            }else{
-                while(j<n && nums[j]<nums[j-1]){
-                    j++;
-                }
+                cnt++;
             }
-            ans=max(ans,j-i);
-            if(j!=n && nums[j]!=nums[j-1])j--;
-            i=j;
+            for(int j=i+1;j<n;j++){
+                if(nums[j]>=nums[j-1]){
+                    break;
+                }
+                cnt++;
+            }
+            ans=max(ans,cnt);
         }
+        
+        
+//         if(n<2)return n;
+//         while(i<n){
+//             while(i+1<n && nums[i]==nums[i+1]){
+//                     i++;
+//             }
+//             int j=i+1;
+//             if(j<n && nums[j]>nums[j-1]){
+//                 while(j<n && nums[j]>nums[j-1]){
+//                     j++;
+//                 }
+//             }else{
+//                 while(j<n && nums[j]<nums[j-1]){
+//                     j++;
+//                 }
+//             }
+//             ans=max(ans,j-i);
+//             if(j!=n && nums[j]!=nums[j-1])j--;
+//             i=j;
+//         }
         return ans;
     }
 };
