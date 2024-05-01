04fd7fef30bce62aebb1dc6d9cf058517de90e14
Deepak pandey <87386712+deepakpandey710@users.noreply.github.com>
2024-05-01 23:18:01 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (65.62%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int i=word.find(ch);
+        if(i!=-1){
+            reverse(word.begin(),word.begin()+i+1);
+        }
+        return word;
+    }
+};
